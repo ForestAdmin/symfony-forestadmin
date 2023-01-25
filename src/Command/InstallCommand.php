d06c9799147f9bb37f697ab6d78bfb476d7e601c
NicolasAlexandre <nicolasalexandre9@gmail.com>
2023-01-25 17:25:34 +0100

    fix(onboarding): set forest_server_url into .env during install  (#8)

--- src/Command/InstallCommand.php
+++ src/Command/InstallCommand.php
@@ -2,16 +2,17 @@
 
 namespace ForestAdmin\SymfonyForestAdmin\Command;
 
-use ForestAdmin\AgentPHP\Agent\Facades\Cache;
 use Illuminate\Support\Str;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\ExceptionInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class InstallCommand extends Command
 {
-    public function __construct(private string $projectDir)
+    public function __construct(private KernelInterface $appKernel)
     {
         parent::__construct();
     }
@@ -28,18 +29,24 @@ class InstallCommand extends Command
      * @param InputInterface  $input
      * @param OutputInterface $output
      * @return int
+     * @throws ExceptionInterface
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $this->deleteDirectory($this->appKernel->getContainer()->getParameter('kernel.cache_dir') . '/forest');
+
         $keys = [
             'FOREST_AUTH_SECRET' => Str::random(32),
             'FOREST_ENV_SECRET'  => $input->getArgument('secretKey'),
         ];
+
+        if (isset($_SERVER['FOREST_SERVER_URL'])) {
+            $keys['FOREST_SERVER_URL'] = $_SERVER['FOREST_SERVER_URL'];
+        }
+
         $this->addKeysToEnvFile($output, $keys, $input->getArgument('envFileName'));
 
         $this->publishConfig($output);
-
-        Cache::forget('config');
 
         return Command::SUCCESS;
     }
@@ -47,7 +54,7 @@ class InstallCommand extends Command
     private function addKeysToEnvFile(OutputInterface $output, array $keys, string $envFileName): void
     {
         foreach ($keys as $key => $value) {
-            file_put_contents($this->projectDir . '/' . $envFileName, PHP_EOL . "$key=$value", FILE_APPEND);
+            file_put_contents($this->appKernel->getProjectDir() . '/' . $envFileName, PHP_EOL . "$key=$value", FILE_APPEND);
         }
         $output->writeln('<info>✅ Env keys correctly set</info>');
     }
@@ -55,12 +62,42 @@ class InstallCommand extends Command
     private function publishConfig(OutputInterface $output)
     {
         $defaultConfigFile = __DIR__ . '/../../config/default.config';
-        $publishFileName = $this->projectDir . '/config/packages/symfony_forest_admin.php';
+        $publishFileName = $this->appKernel->getProjectDir() . '/config/packages/symfony_forest_admin.php';
         if (! file_exists($publishFileName)) {
             copy($defaultConfigFile, $publishFileName);
             $output->writeln('<info>✅ Config file set</info>');
         } else {
             $output->writeln('<info>⚠️  Forest Admin config file already setup</info>');
         }
+    }
+
+    private function deleteDirectory($dirname)
+    {
+        // Sanity check
+        if (! file_exists($dirname)) {
+            return false;
+        }
+
+        // Simple delete for a file
+        if (is_file($dirname) || is_link($dirname)) {
+            return unlink($dirname);
+        }
+
+        // Loop through the folder
+        $dir = dir($dirname);
+        while (false !== $entry = $dir->read()) {
+            // Skip pointers
+            if ($entry == '.' || $entry == '..') {
+                continue;
+            }
+
+            // Recurse
+            $this->deleteDirectory($dirname . DIRECTORY_SEPARATOR . $entry);
+        }
+
+        // Clean up
+        $dir->close();
+
+        return rmdir($dirname);
     }
 }
