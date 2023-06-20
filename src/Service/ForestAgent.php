fa284c296494eb3a6007f6bea9651548b06ad1c6
NicolasAlexandre <nicolasalexandre9@gmail.com>
2023-06-20 11:03:30 +0200

    fix(composer): replace all beta versions with stable versions of packages (#21)

--- src/Service/ForestAgent.php
+++ src/Service/ForestAgent.php
@@ -102,15 +102,16 @@ class ForestAgent implements RouteLoaderInterface
     private function loadOptions(): array
     {
         return [
-            'debug'           => Env::get('FOREST_DEBUG', true),
-            'authSecret'      => Env::get('FOREST_AUTH_SECRET'),
-            'envSecret'       => Env::get('FOREST_ENV_SECRET'),
-            'forestServerUrl' => Env::get('FOREST_SERVER_URL', 'https://api.forestadmin.com'),
-            'isProduction'    => Env::get('APP_ENV', 'dev') === 'prod',
-            'prefix'          => Env::get('FOREST_PREFIX', 'forest'),
-            'cacheDir'        => $this->appKernel->getContainer()->getParameter('kernel.cache_dir') . '/forest',
-            'schemaPath'      => $this->appKernel->getProjectDir() . '/.forestadmin-schema.json',
-            'projectDir'      => $this->appKernel->getProjectDir(),
+            'debug'                => Env::get('FOREST_DEBUG', true),
+            'authSecret'           => Env::get('FOREST_AUTH_SECRET'),
+            'envSecret'            => Env::get('FOREST_ENV_SECRET'),
+            'forestServerUrl'      => Env::get('FOREST_SERVER_URL', 'https://api.forestadmin.com'),
+            'isProduction'         => Env::get('APP_ENV', 'dev') === 'prod',
+            'prefix'               => Env::get('FOREST_PREFIX', 'forest'),
+            'permissionExpiration' => Env::get('FOREST_PERMISSIONS_EXPIRATION_IN_SECONDS', 1),
+            'cacheDir'             => $this->appKernel->getContainer()->getParameter('kernel.cache_dir') . '/forest',
+            'schemaPath'           => $this->appKernel->getProjectDir() . '/.forestadmin-schema.json',
+            'projectDir'           => $this->appKernel->getProjectDir(),
         ];
     }
 }
